1107711089f1a5a9c5a0b9503550635a491fdafb
Alberto Fanjul <albertofanjul@gmail.com>
2021-03-24 10:45:21 +0100

    Add CLI help and version

--- Ryujinx/Program.cs
+++ Ryujinx/Program.cs
@@ -38,6 +38,7 @@ namespace Ryujinx
             string baseDirPathArg     = null;
             bool   startFullscreenArg = false;
             bool   listGames = false;
+            bool   showVersion = false;
 
             for (int i = 0; i < args.Length; ++i)
             {
@@ -53,6 +54,22 @@ namespace Ryujinx
                     }
 
                     baseDirPathArg = args[++i];
+                }
+                else if (arg == "-v" || arg == "--version")
+                {
+                    showVersion = true;
+                }
+                else if (arg == "-h" || arg == "--help")
+                {
+                    Console.Write(@"Ryujinx [options] [file]
+
+-h, --help         Show this help
+-v, --version      Show Ryujinx version
+-f, --fullscreen   Run in fullscreen
+    --list-games   List Available games in json format
+
+");
+		    return;
                 }
                 else if (arg == "-f" || arg == "--fullscreen")
                 {
@@ -81,6 +98,11 @@ namespace Ryujinx
             });
 
             Version = Assembly.GetEntryAssembly().GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion;
+	    if (showVersion)
+	    {
+		    Console.Write(Version);
+		    return;
+	    }
 
             Console.Title = $"Ryujinx Console {Version}";
 
@@ -158,7 +180,7 @@ namespace Ryujinx
                     gameMetadataArray.Add(data);
                 };
                 applicationLibrary.LoadApplications(ConfigurationState.Instance.Ui.GameDirs, ConfigurationState.Instance.System.Language);
-                System.Console.Write(JsonHelper.Serialize(gameMetadataArray, true));
+                Console.Write(JsonHelper.Serialize(gameMetadataArray, true));
                 return;
             }
 
